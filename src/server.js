76e4621f1ee48cf11f347a22e7a961af2e283b49
ChuppySuidae <100174761+ChuppySuidae@users.noreply.github.com>
2024-05-08 20:48:34 +0700

    init code

---
+++ src/server.js
@@ -0,0 +1,12 @@
+// const express = require('express')
+import express from 'express'
+const app = express()
+const hostname =  'localhost'
+const port = 8017
+
+app.get('/', function(reg, res){
+  res.send('<h1>Hello World Nodejs</h1>')
+})
+app.listen(port, hostname, () => {
+  console.log(`Hello, I'm running server at http://${hostname}:${port}/`)
+})
